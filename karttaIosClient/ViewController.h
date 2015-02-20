13098265dfcce3c16a51960b20a112352d60ea7e
Leif Roschier <leif.roschier@iki.fi>
2015-02-21 00:49:02 +0200

    First steps of location and server

--- karttaIosClient/ViewController.h
+++ karttaIosClient/ViewController.h
@@ -8,15 +8,22 @@
 
 #import <UIKit/UIKit.h>
 #import <SIOSocket/SIOSocket.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController    
+@interface ViewController : UIViewController < CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *chatMessage;
 
 @property (weak, nonatomic) IBOutlet UITextField *serverAddress;
 @property (weak, nonatomic) IBOutlet UITextField *userName;
 @property (weak, nonatomic) IBOutlet UITextField *trackRoom;
+
+// socket
 @property SIOSocket *socket;
+@property (nonatomic, strong) NSString *socketID;
+
+// for location tracking
+@property (nonatomic, strong) CLLocationManager *locationManager;
 
 
 @end
