2ffab7183e54944f540661e1354b71534700adb1
Leif Roschier <leif.roschier@iki.fi>
2015-02-20 23:35:51 +0200

    sends chat message

--- karttaIosClient/ViewController.h
+++ karttaIosClient/ViewController.h
@@ -7,8 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <SIOSocket/SIOSocket.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController    
+
+@property (weak, nonatomic) IBOutlet UITextField *chatMessage;
+
+@property (weak, nonatomic) IBOutlet UITextField *serverAddress;
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *trackRoom;
+@property SIOSocket *socket;
 
 
 @end
--- karttaIosClient/ViewController.m
+++ karttaIosClient/ViewController.m
@@ -17,11 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [SIOSocket socketWithHost: [NSString stringWithFormat:@"http://%@", _serverAddress.text] response: ^(SIOSocket *socket) {
+        self.socket = socket;
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)sendChatMessage:(id)sender {
+    NSLog(_chatMessage.text);
+    [self.socket emit: @"kMessage" args: @[@{@"text" : _chatMessage.text,
+                                             @"type" : @"textMessage",
+                                             @"name" : _userName.text,
+                                             @"trackroom" : _trackRoom.text
+                                             }]];
+
+}
+
 
 @end
